5afaf1c4cb18a763052435c991ca0288443c9def
Stray-God <kowshikdiamonds@gmail.com>
2022-04-18 22:52:22 +0530

    import movie data

---
+++ src/mdata/index.js
@@ -0,0 +1,46 @@
+const cdata = [
+    {
+        imgSrc: 'https://static.toiimg.com/photo/msid-90508268/90508268.jpg?180854',
+        
+        title:'RRR is a period drama set in India during the 1920s, revolving around the inspiring journey of two of India`s freedom fighters - Alluri Sitarama Raju and Komaram Bheem.',
+        sname:'RRR',
+        link:'https://in.bookmyshow.com/hyderabad/movies/rrr/ET00094579'
+      
+    },
+    {
+        imgSrc:'https://static.toiimg.com/photo/90612493.jpeg',
+        title:'Veera Raghavan, a former raw agent, is tasked to deal with an International Terrorist organization who have hijacked a mall demanding the release of their leader. Can Veera rescue all the hostages in time?',
+        sname:'BEAST',
+        link:'https://in.bookmyshow.com/hyderabad/movies/beast/ET00311733', 
+    },
+    {
+        imgSrc:'https://www.tollywood.net/wp-content/uploads/2022/01/KGF-Chapter-2-actors-remuneration-list.jpg',
+        
+        title:' The blood-soaked land of Kolar Gold Fields (KGF) has a new overlord now - Rocky, whose name strikes fear in the heart of his foes. His allies look up to Rocky as their saviour, the government sees him as a threat to law and order; enemies are clamouring for revenge and conspiring for his downfall. Bloodier battles and darker days await as Rocky continues on his quest for unchallenged supremacy.',
+        sname:'K.G.F. Chapter 2',
+        link:'https://in.bookmyshow.com/hyderabad/movies/kgf-chapter-2/ET00098647'
+    },
+    {
+        imgSrc:'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202202/pjimage_33.jpg?sKm1.Bt3oWjz1lKCIV0R0Yc1BfSj43Kc&size=770:433',
+        title:' Doctor Strange in the Multiverse of Madness - a thrilling ride through the Multiverse with Doctor Strange, his trusted friend Wong and Wanda Maximoff, aka Scarlet Witch.',
+        sname:'Doctor Strange: In The Multiverse Of Madness',
+        link:'https://in.bookmyshow.com/hyderabad/movies/doctor-strange-in-the-multiverse-of-madness/ET00310791'
+      
+    },
+    {
+        imgSrc:'https://pbs.twimg.com/media/FQAOkfSXoAgUtYs.jpg',
+        title:' Professor Albus Dumbledore (Jude Law) knows that the powerful Dark wizard Gellert Grindelwald (Mads Mikkelsen) is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts Magizoologist Newt Scamander (Eddie Redmayne) to lead an intrepid team of wizards, witches and one brave Muggle baker on a dangerous mission, where they encounter old and new beasts and clash with Grindelwald`s growing legion of followers. But with the stakes so high, how long can Dumbledore remain on the sidelines?',
+        sname:'Fantastic Beasts: The Secrets Of Dumbledore',
+        link:'https://in.bookmyshow.com/hyderabad/movies/fantastic-beasts-the-secrets-of-dumbledore/ET00319643'
+      
+    },
+    {
+    imgSrc: 'https://assets.thehansindia.com/h-upload/2021/12/11/1130219-gani.webp',
+    title:' Ghani is a Telugu movie starring Saiee Manjrekar, Varun Tej and Naveen Chandra in prominent roles. It is a drama directed by Kiran Korrapati with S. Thaman as musician, forming part of the crew.',
+    sname:'GHANI',
+    link:'https://in.bookmyshow.com/hyderabad/movies/ghani/ET00305453'
+  
+    }
+]
+
+export default cdata;
