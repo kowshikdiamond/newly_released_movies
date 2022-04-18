c112912bad1695a57b7189c0431b0fc794b296b7
Stray-God <kowshikdiamonds@gmail.com>
2022-04-18 23:19:00 +0530

    adjust data path

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Card from '../../components/Card';
-import cdata from '../../../src/cdata';
+import mdata from '../../../src/mdata';
 
-// console.log(cdata[1].title);
+// console.log(mdata[1].title);
 const App = () => {
   return (
     <>
       <h1 className='heading_style'>List of Newly Released Movies</h1>
       <Card
-        imgSrc={cdata[0].imgSrc}
-        title={cdata[0].title}
-        link={cdata[0].link}
-        sname={cdata[0].sname}
+        imgSrc={mdata[0].imgSrc}
+        title={mdata[0].title}
+        link={mdata[0].link}
+        sname={mdata[0].sname}
       />
       { <Card
         link=' https://in.bookmyshow.com/hyderabad/movies/spiderman-no-way-home/ET00310790'
@@ -20,34 +20,34 @@ const App = () => {
         title="When Dr. Strange`s spell to restore Spider-Man`s identity goes awry, Peter is forced to overcome his greatest challenge yet."
       /> }
       <Card
-        imgSrc={cdata[1].imgSrc}
-        title={cdata[1].title}
-        link={cdata[1].link}
-        sname={cdata[1].sname}
+        imgSrc={mdata[1].imgSrc}
+        title={mdata[1].title}
+        link={mdata[1].link}
+        sname={mdata[1].sname}
       />
       <Card
-        imgSrc={cdata[2].imgSrc}
-        title={cdata[2].title}
-        link={cdata[2].link}
-        sname={cdata[2].sname}
+        imgSrc={mdata[2].imgSrc}
+        title={mdata[2].title}
+        link={mdata[2].link}
+        sname={mdata[2].sname}
       />
       <Card
-        imgSrc={cdata[3].imgSrc}
-        title={cdata[3].title}
-        link={cdata[3].link}
-        sname={cdata[3].sname}
+        imgSrc={mdata[3].imgSrc}
+        title={mdata[3].title}
+        link={mdata[3].link}
+        sname={mdata[3].sname}
       />
       <Card
-        imgSrc={cdata[4].imgSrc}
-        title={cdata[4].title}
-        link={cdata[4].link}
-        sname={cdata[4].sname}
+        imgSrc={mdata[4].imgSrc}
+        title={mdata[4].title}
+        link={mdata[4].link}
+        sname={mdata[4].sname}
       />
       <Card
-        imgSrc={cdata[5].imgSrc}
-        title={cdata[5].title}
-        link={cdata[5].link}
-        sname={cdata[5].sname}
+        imgSrc={mdata[5].imgSrc}
+        title={mdata[5].title}
+        link={mdata[5].link}
+        sname={mdata[5].sname}
       />
     </>
 
